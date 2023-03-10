c8e9b06a16d57df58adcd0829e7a49ccee66215e
Alexandre Marra <alexsmarra@hotmail.com>
2023-03-10 19:49:12 -0300

    using children

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,13 +1,25 @@
 // importando de styles.js
-import { Container } from "./styles"
+import { Container, Links } from "./styles"
 import { Header } from "../../components/Header"
 import { Button } from "../../components/Button"
-
+import { Section } from "../../components/Section"
+ 
 export function Details() {
   return (
     // encapsulando o que foi importado para funcionar o CSS
     <Container>
       <Header/>
+      <Section title="Links úteis">
+        {/* tudo aqui dentro é referente ao "children" */}
+        <Links>
+          <li>
+            <a href="#">https://www.rocketseat.com.br/</a>
+          </li>
+          <li>
+            <a href="#">https://www.rocketseat.com.br/</a>
+          </li>
+        </Links>
+      </Section>
       <Button title="voltar"/>
     </Container>
   )
