65e232bf6e67362208b547fe201690c278c61609
Alexandre Marra <alexsmarra@hotmail.com>
2023-03-10 10:59:32 -0300

    primieras estilizacoes para o header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,16 @@
-import { Container } from "./styles"
+import { Container, Profile } from "./styles"
+
 
 export function Header() {
    return (
       <Container>
-         
+         <Profile>
+            <img src="https://github.com/alexsmarra.png" alt="User img" />
+            <div>
+               <span>Welcome,</span>
+               <strong>Alexandre Simões Marra</strong>
+            </div>
+         </Profile>
       </Container>
    )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -10,6 +10,31 @@ export const Container = styled.header`
    display: flex;
    justify-content: space-between;
    padding: 0 80px;
-   background: brown; 
 `
+export const Profile = styled.div`
+   display: flex;
+   align-items: center;
+   /* esse sinal de ">" é para garantir que vamos estilizar somente a img que está dentro de Profile */
+   > img {
+      width: 56px;
+      height: 56px;
+      border-radius: 50%;
+   }
 
+   > div {
+      display: flex;
+      flex-direction: column; 
+      margin-left: 16px;
+      line-height: 24px;
+      /* span dentro da div */
+      span {
+         font-size: 14px;
+         color: ${({ theme }) => theme.COLORS.GRAY_100}
+      }
+      /* para strong dentro da div */
+      strong {
+         font-size: 18px;
+         color: ${({ theme }) => theme.COLORS.WHITE}
+      }
+   }
+`
