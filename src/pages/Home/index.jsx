18036b4e18c78651cd5758794b5e01eceab18c5b
Alexandre Marra <alexsmarra@hotmail.com>
2023-03-16 16:43:45 -0300

    Note component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,8 @@ import { Container, Brand, Menu, Search, Content, NewNote } from "./styles.js"
 import { Header } from "../../components/Header"
 import { ButtonText } from "../../components/ButtonText"
 import { Input } from "../../components/Input"
+import { Section } from '../../components/Section'
+import { Note } from '../../components/Note'
 
 
 export function Home() {
@@ -25,7 +27,16 @@ export function Home() {
          </Search>
 
          <Content>
-
+            <Section title="Minhas notas">
+               <Note data={{
+                  title: 'React',
+                  tags: [
+                     {id: '1', name: 'react'},
+                     {id: '2', name: 'rocketseat'}
+                  ]
+               }}
+               />
+            </Section>
          </Content>
 
          <NewNote>
