a610f3843ad07a638cb282b0754cfc7985dec238
Alexandre Marra <alexsmarra@hotmail.com>
2023-03-17 12:09:09 -0300

    SignIn structure [estrutura do SignIn]

--- src/components/Note/index.jsx
+++ src/components/Note/index.jsx
@@ -1,7 +1,7 @@
 import { Container } from './styles'
 import { Tag } from '../../components/Tag'
 
-export function Note({ data, ...rest}) {
+export function Note({ data, ...rest }) {
    return (
       <Container {...rest}>
          <h1>{data.title}</h1>
@@ -11,6 +11,7 @@ export function Note({ data, ...rest}) {
                <footer>
                   {
                      data.tags.map(tag => {
+                        // in lists the key is recommended for application performance, react uses keys to render only what is needed. 
                         return <Tag key={tag.id} title={tag.name}/>
                      })
                   }
