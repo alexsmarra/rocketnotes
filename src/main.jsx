9c8a0084c691c763f974fa1427b882ef25163822
Alexandre Marra <alexsmarra@hotmail.com>
2023-03-20 17:57:51 -0300

    SignUp

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,14 @@ import { ThemeProvider } from "styled-components"
 // O nosso tema. Em nosso root mais abaixo, encapsulamos o conteúdo com o "<ThemeProvider>" e colocamos o "theme={theme}", para dessa forma compartilhar para todo o app o conteúdo da pasta "./styles/theme.js"
 import theme from "./styles/theme"
 import GlobalStyles from "./styles/global"
-import { SignIn } from './pages/SignIn'
+import { SignUp } from './pages/SignUp'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <SignIn />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>,
 )
