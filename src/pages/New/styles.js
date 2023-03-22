7625b66d4ee0e8722b2bc9161159eb548dac0885
Alexandre Marra <alexsmarra@hotmail.com>
2023-03-22 11:48:17 -0300

    New page and another components

--- src/pages/New/styles.js
+++ src/pages/New/styles.js
@@ -2,13 +2,24 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
    width: 100%;
-   height: 100%;
+   height: 100vh;
 
    display: grid;
    grid-template-rows: 105px auto;
    grid-template-areas: 
       "header"
       "content";
+
+   > main {
+      grid-area: content;
+      overflow-y: auto;
+   }
+
+   .tags {
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+   }
 `
 
 export const Form = styled.form`
