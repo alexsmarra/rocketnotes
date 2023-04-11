29ae2cd81c8d5152392ec55aedb037164dff3e85
Alexandre Marra <alexsmarra@hotmail.com>
2023-04-11 18:24:35 -0300

    instalando axios e iniciando funcoes em SignUp para pegar dados do usuario digitado nos inputs

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,3 +1,7 @@
+// create states to collect data entered by the user in the user registration field (name, email, password..)
+import { useState } from "react"
+import { api } from "../../services/api"
+
 import { Container, Form, Background } from './styles'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
@@ -5,6 +9,17 @@ import { FiMail, FiLock, FiUser } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 
 export function SignUp() {
+   // initial value is empty ""
+   const [name, setName] = useState("")
+   const [email, setEmail] = useState("")
+   const [password, setPassword] = useState("")
+
+   function handleSignup() {
+      if(!name || !email || !password) {
+         return alert("Fill in all fields!")
+      }
+   }
+
    return (
       <Container>
          <Background />
@@ -18,19 +33,22 @@ export function SignUp() {
                placeholder="Nome"
                type="text"
                icon={FiUser}
+               onChange={e => setName(e.target.value)}
             />
             <Input 
                placeholder="E-mail"
                type="text"
                icon={FiMail}
+               onChange={e => setEmail(e.target.value)}
             />
             <Input 
                placeholder="Senha"
                type="password"
                icon={FiLock}
+               onChange={e => setPassword(e.target.value)}
             />
 
-            <Button title="Cadastrar" />
+            <Button title="Cadastrar" onClick={handleSignup}/>
             
             <Link to="/">
                Voltar para o login
